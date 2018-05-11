756c501cacee03962e8b3d1f2ad766ce2e305494
trivediu <31199718+trivediu@users.noreply.github.com>
2018-05-10 18:27:53 -0700

    Version 4

--- TrivediUday_HW6_V4.asm
+++ TrivediUday_HW6_V4.asm
@@ -29,12 +29,6 @@ mWriteStrLine	MACRO	buffer							;same as above macro, but does not have a line 
 	pop edx												;restore the edx register
 ENDM
 
-mWriteFact		MACRO									
-		mov dwordeax,eax
-		fild dwordeax
-		call WriteFloat
-ENDM
-
 
 .data
 ; (insert variable definitions here)
@@ -42,11 +36,25 @@ ENDM
 	;String Variable Definitions
 	introPlaceholder	BYTE	"Some intro stuff goes here. Enter a number: ",0
 	problemTitle		BYTE	"Problem #: ",0
-	problemLine1		BYTE	"NUmber of elements in the set: ",0
-	problemLine2		BYTE	"Number of elements to choose from the set: ",0
+	problemLine1		BYTE	"NUmber of elements in the set (n): ",0
+	problemLine2		BYTE	"Number of elements to choose from the set (r): ",0
 	problemLine3		BYTE	"How many ways can you choose? ",0
 	tInput				BYTE	21 DUP (0)		;temporary value to hold user input 
-	error				BYTE	"ERROR!",0
+	error				BYTE	"ERROR! Please try again",0
+	userChoice			BYTE	5 DUP (?)			;create a placeholder for y/n
+
+	showResult1			BYTE	"There are ",0
+	showResult2			BYTE	" combinations of ",0
+	showResult3			BYTE	" items from a set of ",0
+	showResultBad		BYTE	"You need more practice.",0
+	showResultGood		BYTE	"You are correct!",0
+	showResultDot		BYTE	".",0
+	showResultEC1a		BYTE	"EXTRA CREDIT AMAZING 1: The sum calculated recursively starting from 'r' to 'n' is: ",0
+	showResultEC2		BYTE	"EXTRA CREDIT AMAZING 2: The FPU-Calculated mean of your answer and the actual answer is: ",0
+	askContinue			BYTE	"Another problem? (y/n): ",0
+	tempEnd				BYTE	"end",0
+	tempStart			BYTE	"start",0
+
 
 	;Numerical (integer and/or float) variable definitions
 	n			real4	0.			;the value of n 
@@ -57,6 +65,12 @@ ENDM
 	dwordeax	DWORD	?			;just a temp dword variable to hold the value in eax
 	answerSize	DWORD	?			;originally was going to be used, but now just a placeholder 
 	result		real4	?			;the correct value of the calculation
+	sum			real4	?			;extra credit, the sum from r to n calculated recursively
+	realTwo		real4	2.			;value of 2
+
+	problemNum	dword	0			;the problem number
+	right		dword	0			;the number of questions right
+	wrong		dword	0			;the number of questions wrong
 
 ;***START PROGRAM HERE***
 .code
@@ -75,7 +89,7 @@ main PROC
 		;INTRO PROCEDURE
 		call intro					;call the intro procedure
 
-
+	_start:							;this label is used to loop if the user chooses to play again
 		;SHOW PROBLEM PROCEDURE
 		push OFFSET n				;push the reference address of n onto stack
 		push OFFSET r				;push the reference address of r onto the stack
@@ -94,15 +108,64 @@ main PROC
 		push n						;[ebp+16] push the value of n by value
 		push r						;[ebp+12] push the value of r by value
 		push OFFSET	result			;[ebp+8]  push the result variable by reference
-		call combinations
+		call combinations			;call the combinations procedure
 
-		;push 12
-		;call factorial
-		;mov dwordeax,eax
-		;fild dwordeax
-		;call WriteFloat
+
+		;showResults procedure
+		push result					;push the value of result
+		push answer					;push the value of the user's answer
+		push n						;push the value of n
+		push r						;push the value of r
+		call showResults			;call the showResults procedure
 
 		
+		;EXTRA CREDIT AMAZING 1: Recursively calculate the sum from r to n
+		push r						;push the value of r
+		push n						;push the value of n
+		push OFFSET sum				;push the offset address of sum
+		call summation				;call the summation procedure
+		call writefloat				;call writeFloat
+
+		;EXTRA CREDIT AMAZING 2: Calculate the mean within the FPU of the user's answer and the actual answer
+		push realTwo				;the divisor
+		push answer					;the answer
+		push result					;the result
+		call average
+
+
+		;Ask the user whether to continue or not (this was not required to be a procedure)
+
+		_YNLoop:	
+			mWriteStrLine askContinue							;print the statement asking user to continue
+			mov edx, OFFSET userChoice							;move offset of userchoice string to read into edx
+			mov ecx, 5											;read up to 5 chars
+			call ReadString										;call readstring procedure
+
+			cmp eax,1											;if user string input is greater than 1, then error
+			jg _err												;jump to error message
+
+			mov esi, OFFSET userChoice							;move the string input offset to esi
+			mov al,[esi]										;move value of userchoice to al
+
+			cmp al,79h											;is al = y?
+			je _start											;then go back to start of program
+
+			cmp al, 59h											;is al = Y?
+			je _start											;then go back to start of program
+
+			cmp al, 6Eh											;is al = n?
+			je _end												;then go to end label of the program
+
+			cmp al, 4Eh											;is al = N?
+			je _end												;then go to end label of the program
+
+		_err:													;error label
+		mWriteStr error											;output errror message											
+		jmp _YNLoop												;jump back to prompt the user again y/n
+
+		_end:													;end label
+		mWriteStr	tempEnd
+	
 	
 		call Crlf
 	exit	; exit to operating system
@@ -381,12 +444,11 @@ getData ENDP
 
 ;************************************************************************************************************
 ; COMBINATIONS PROCEDURE
-; prompts / gets the user’s answer.
-; receives: the OFFSET of answer and temp and value of answerSize
-; returns: none
+; calulates the correct value of combvinations by calling the recursive factoral function
+; receives: the value of n,r, and offset of result
+; returns: updated result via reference
 ; preconditions: none
-; registers changed:  eax, ebx, ecx, edx, ebp, esi, esp
-; resources used: stackoverflow.com/questions/13664778/converting-string-to-integer-in-masm-esi-difficulty
+; registers changed:  eax, ebx, esi
 ;************************************************************************************************************
 combinations PROC
 	;ebp+16 = n
@@ -397,7 +459,7 @@ combinations PROC
 	
 		mov ebp,esp						;move esp into ebp
 	
-		sub esp,20						;make space for four local variables
+		sub esp,20						;make space for five local variables
 
 	;[STEP 1] calculate n!
 	
@@ -448,14 +510,226 @@ combinations PROC
 										;now st(0) holds n! while st(1) holds the denominator r!(n-r)!
 
 		fdiv st(0),st(1)				;divide numerator by the denominator and store result in st(0)
-		call writeFloat
+										;now the official result is stored in st(0)
+		
+		mov esi, [ebp+8]				;mov address of result into esi				
+		fistp dword ptr [esi]			;mov st(0) into esi, which is offset address of result
+										;the value of "result" is now updated
+
+
+	mov esp,ebp							;restore value of ebp back to esp
+    pop     ebp							;pop esp
+    ret     12						
+combinations ENDP
 
 
 
+
+;************************************************************************************************************
+; SHOWRESULTS PROCEDURE
+; displays the correct results and gives an opinion about the user's answer
+; receives: the value of result, answer, n, r
+; returns: none
+; preconditions: none
+; registers changed:  eax, 
+;************************************************************************************************************
+showResults PROC
+		;VARIABLES used
+		;push result [ebp+20]
+		;push answer [ebp+16]
+		;push n		 [ebp+12]
+		;push r		 [ebp+8]
+		
+		push ebp								;push ebp onto stack
+		mov ebp,esp								;move esp into ebp
+		sub esp,20								;make space for five local variables
+
+		call Crlf
+		mWriteStrLine		showResult1			;"There are "
+		fild dword ptr		[ebp+20]			;move result from real4 format into 
+		call WriteFloat
+
+		mWriteStrLine		showResult2			; " combinations of "
+		fild dword ptr	    [ebp+8]				;move r from real4 format into fpu
+		call WriteFloat
+
+		mWriteStrLine		showResult3			; "from a set of "
+		fild dword ptr		[ebp+12]			;move n from real4 format into fpu
+		call WriteFloat
+
+		mWriteStr			showResultDot		;"."
+
+		mov eax,[ebp+16]						;move the answer into eax
+		mov ebx,[ebp+20]						;move the official result into ebx
+
+		cmp eax,ebx
+		je _Success
+
+		_Fail:
+			mWriteStr showResultBad					;display a message informing user to improve
+			jmp _End
+
+		_Success:
+			mWriteStr showResultGood				;display a message informing user is correct
+
+		_End:
+		mWriteStrLine showResultEC1a				;"Extra Credit the sum from r to n is: ";
+
+		
+		
 	mov esp,ebp
     pop     ebp
+    ret     16
+showResults ENDP
+
+
+
+;****************************************************************************************
+;RECURSIVE procedure to calculate the summation of integers from x to y.
+;Implementation note: This procedure implements the following
+;	recursive algorithm:
+;	if (x == y)
+;		return x
+;	else
+;		return x + summation(x+1,y)
+;receives: values of x and y, address of result ... on the system stack
+;returns: sum = x + sum of (x+1)+ ... +y
+;preconditions:  x <= y
+;registers changed: eax,ebx,edx
+;
+;**************************************************************************************
+
+summation	PROC
+	push	ebp
+	mov		ebp,esp
+	finit
+	mov		eax,[ebp+16]	;eax = r
+	mov		ebx,[ebp+12]	;ebx = n
+	mov		edx,[ebp+8]		;@sum in edx
+	
+	add		[edx],eax		;add current value of x
+	cmp		eax,ebx
+	je		quit			;base case: sum = x
+recurse:
+	inc		eax				;recursive case
+	push	eax
+	push	ebx
+	push	edx
+	call	summation
+quit:
+	pop		ebp
+	fild  real4 ptr	[edx]	;load sum into the fpu
+	ret		12
+summation	ENDP
+
+
+
+
+;****************************************************************************************
+;AVERAGE procedure will calculate the mean of the user's answer and the correct answer
+;receives: values of answer and result and outputs the mean value
+;returns: nothing
+;preconditions:  answer and result must exit
+;registers changed: eax
+;**************************************************************************************
+
+average	PROC
+	;push 2.0 [ebp+16]
+	;push answer [ebp+12]
+	;push result [ebp+8]
+	
+	push	ebp						;push ebp
+	mov		ebp,esp					;set esp value into ebp
+
+	finit							;reinitialize
+	
+	fild real4 ptr [ebp+12]			;load value of answer into fpu
+	
+	fild real4 ptr [ebp+8]			;load value of result into fpu
+	
+	fadd							;add the first two portions of fpu stack st(0) + st(1)
+	
+	fld dword ptr [ebp+16]			;load the value of 2.
+	
+	fdiv							;divide the sum by 2
+	
+	call Crlf						;line break
+	
+	mWriteStrLine showResultEC2		;output result
+	
+	call writeFloat					;write st(0) to output
+	
+	call Crlf						;line break
+
+quit:
+	pop		ebp
+	ret		12
+average	ENDP
+
+
+
+
+
+;************************************************************************************************************
+; GETDATA2 PROCEDURE
+; prompts / gets the user’s answer.
+; receives: the OFFSET of answer and temp and value of answerSize
+; returns: none
+; preconditions: none
+; registers changed:  eax, ebx, ecx, edx, ebp, esi, esp
+; resources used: stackoverflow.com/questions/13664778/converting-string-to-integer-in-masm-esi-difficulty
+;************************************************************************************************************
+getData2 PROC
+	push ebp						;push ebp onto stack
+		
+	mov ebp,esp						;move esp into ebp
+
+LoopA:
+	mWriteStrLine   problemLine3	;print the part that prompts the user to input their answer
+	
+	;call Crlf						;insert line break over here
+
+	;mov	edx, [ebp+16]			;move offset of the tInput for the string of ints to be read into
+
+	mov ecx, 1						;move the value of 1 into ecx register					
+
+	call ReadString					;call the irvine proc to read the string
+
+	cmp eax, 1						;make sure string size does not exceed 1
+
+	jg invalidInput					;if string size is greater than 10 jump to invalidInput
+
+	mov al, [esi]
+	cmp al,79h
+	je _start
+	jmp invalidInput
+
+	_start:
+	mwritestr tempStart
+
+	
+	
+	
+	;popad
+	
+	jmp     moveOn
+
+invalidInput:						;set reg and variabls values to zero
+    mov     al,0
+    
+	mov     eax,0					;mov 0 to the eax register
+    
+    mov     [ebx],eax				;mov the eax value into [ebx]
+    
+	mWriteStr   error				;output an error message
+
+    jmp     LoopA					;jump back to the beginning of loopA
+moveOn:
+    pop     ebp
     ret     12
-combinations ENDP
+getData2 ENDP
+
+
 ;//**************************************************************************************
 ;// (insert additional procedures here)
 
